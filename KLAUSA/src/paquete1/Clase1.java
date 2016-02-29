f21fa8232b2c7e568191dcc8d4a59b7eccb898bc
Alvaro <admin@IC08.cesbur>
2016-02-29 11:52:54 +0100

    He añadido un método

--- KLAUSA/src/paquete1/Clase1.java
+++ KLAUSA/src/paquete1/Clase1.java
@@ -1,5 +1,11 @@
 package paquete1;
 
+import javax.swing.JOptionPane;
+
 public class Clase1 {
+	
+	public void main (String [] args) {
+		JOptionPane.showMessageDialog(null, "Hola Klaus");
+	}
 
 }
